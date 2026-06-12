aad8ed953a2fe30800de7b5ea4408cb90287c864
allpass83 <winisme0803@gmail.com>
2026-06-12 16:58:34 +0800

    Update firebase-pass.js

--- firebase-pass.js
+++ firebase-pass.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAmNPgtlowEM7DF1jbwQWCItozZzt8tzWc",
+  authDomain: "pass-credit-system.firebaseapp.com",
+  projectId: "pass-credit-system",
+  storageBucket: "pass-credit-system.firebasestorage.app",
+  messagingSenderId: "938198180976",
+  appId: "1:938198180976:web:e1c38afef59c819a5440b5"
+};
